1521cc1d87cdbaeb87ac4605566e1aaa644b654a
Harry Lawrence <harry.lawrence@groundsix.com>
2013-04-23 23:48:17 +0100

    Added comment, min.js and used that for example.

--- jQuery-KeyBridge-0.1.js
+++ jQuery-KeyBridge-0.1.js
@@ -1,3 +1,4 @@
+/* jQuery Key Bridge - Developed by Harry Lawrence - hazbo.github.com */
 (function( $ ){
     $.fn.keyBridge = function(key) {
         jQuery(this).trigger({ type: 'keypress', which: key.charCodeAt(0) });
